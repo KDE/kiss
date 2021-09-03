22cf331deb82116fea63fd8e6529b1b30022e0ec
Jan Blackquill <uhhadd@gmail.com>
2021-09-02 21:07:03 -0400

    Licensing information

--- src/kiss.cpp
+++ src/kiss.cpp
@@ -1,7 +1,8 @@
-// SPDX-FileCopyrightText: 2021 Carson Black <uhhadd@gmail.com>
 // SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+// SPDX-FileCopyrightText: 2021 Carson Black <uhhadd@gmail.com>
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "kiss.h"
 
--- src/settings.h
+++ src/settings.h
@@ -1,7 +1,9 @@
-// SPDX-FileCopyrightText: 2021 Carson Black <> // TODO
 // SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+// SPDX-FileCopyrightText: 2021 Carson Black <> // TODO
+// SPDX-FileCopyrightText: 2021 Carson Black <uhhadd@gmail.com>
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
 
