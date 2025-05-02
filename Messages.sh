4b2ca3a2f967f01c774692401a83b85cd5aac3e2
Carl Schwan <carl@carlschwan.eu>
2025-05-03 01:11:56 +0200

    Use normal Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 
-#!/bin/sh find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml | $XGETTEXT -o "$podir/org.kde.initialsystemsetup.pot" -f -
+$XGETTEXT `find -name \*.cpp -o -name \*.qml` -o "$podir/org.kde.initialsystemsetup.pot"
